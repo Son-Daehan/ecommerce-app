efb015ae61b9e042d3f17b37b66763f29cbde7bc
Samuel <Samuel.Daehan@gmail.com>
2022-05-06 17:08:06 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-
+User.create!([
+  {name: "Samuel", email: "Samuel@gmail.com", password_digest: "$2a$12$.UdOu/.jt3SDzVl1TKMk/.lskz4LWXOr3EBPOe3PveL2Ar1pnCZHW", admin: nil, phone_number: nil}
+])
 Product.create!([
   {title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", price: "109.95", description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", category: "men's clothing", image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"},
   {title: "Mens Casual Premium Slim Fit T-Shirts ", price: "22.3", description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.", category: "men's clothing", image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"},
@@ -20,6 +22,9 @@ Product.create!([
   {title: "MBJ Women's Solid Short Sleeve Boat Neck V ", price: "9.85", description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem", category: "women's clothing", image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg"},
   {title: "Opna Women's Short Sleeve Moisture", price: "7.95", description: "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort", category: "women's clothing", image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"},
   {title: "DANVOUY Womens T Shirt Casual Cotton Short", price: "12.99", description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.", category: "women's clothing", image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"}
+])
+UserAddress.create!([
+  {street_name: "192 Holland St", city: "Northfield", region: "Illinois", country: "USA", postal_code: "60062", user_id: 1, tax_id: 1, ship_to: true}
 ])
 Category.create!([
   {name: "men's clothing"},
